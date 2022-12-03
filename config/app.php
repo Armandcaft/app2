16f3e7dc50a1f52f51c007e2ee744a538740427f
Armandcaft <71290581+Armandcaft@users.noreply.github.com>
2022-12-03 21:36:35 +0100

    added debugbar

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -211,5 +212,6 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
+    'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
 
 ];
