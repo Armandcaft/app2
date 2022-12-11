f78fad24207c9a0353f35a6348baaf59b93fd761
Armandcaft <71290581+Armandcaft@users.noreply.github.com>
2022-12-11 04:57:23 +0100

    update

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -26,12 +26,14 @@
             <hr class="border border-1 border-gray-300 mt-10">
         </div>
 
-        <div class="py-10 sm:py-20">
-            <a class="primary-btn inline text-base sm:text-xl bg-green-500 py-4 px-4 shadow-xl rounded-full transition-all hover:bg-green-400"
-               href="{{ route('blog.create') }}">
-                New Article
-            </a>
-        </div>
+        @if (Auth::user())
+            <div class="py-10 sm:py-20">
+                <a class="primary-btn inline text-base sm:text-xl bg-green-500 py-4 px-4 shadow-xl rounded-full transition-all hover:bg-green-400"
+                href="{{ route('blog.create') }}">
+                    New Article
+                </a>
+            </div>
+        @endif
     </div>
 
     @if (session()->has('message'))
