74146dfc2e2ad9209abf8ee9cd05bf0536ac7d7f
Armandcaft <71290581+Armandcaft@users.noreply.github.com>
2023-01-12 04:59:39 +0100

    up

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -64,9 +64,9 @@
                     <a
                         href=""
                         class="font-bold text-green-500 italic hover:text-green-400 hover:border-b-2 border-green-400 pb-3 transition-all py-20">
-                        Code With Dary
+                        Code With Stormeur
                     </a>
-                    On 17-07-2022
+                    On 03-01-2023
                 </span>
             </div>
         </div>
